867e7577fa75645e88a68069c597a3d11a1e48ed
mugeshkumar <kmugesh11199@gmail.com>
2022-08-22 12:34:53 +0530

    add new file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*value assignment*/
+
+/*let a=55;
+console.log(a);
+ a=10;
+console.log(a);
+
+const a=10;
+console.log(a);
+a=20;
+console.log(a);*/
+
+const student={"name":"mani","age"}
+
+
+
+
+
+
