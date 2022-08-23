868a2af191f0ca1a90a1216f7d7c3ca94f365515
mugeshkumar <kmugesh11199@gmail.com>
2022-08-23 18:41:52 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,60 @@
-/*value assignment*/
-
-/*let a=55;
-console.log(a);
- a=10;
-console.log(a);
-
-const a=10;
-console.log(a);
-a=20;
-console.log(a);*/
-
-const student={"name":"mani","age"}
+/*var state 
+var country ="india";
+var language ="tamil"
+if (language ="tamil") {
+	state="tamilnadu";
+}
+if (language="hindi"){
+	state ="delhi"
+}
+else {
+state="andhra";}
 
 
-
+/** end 
+if (country="india")
+	if (language=="tamil"){
+		state="tamilnadu";
+	}
+	else if (language=="hindi"){
+		state="delhi";
+	}
+	/** end 
+	/**nested if 
+	var a=10;
+	var b="10";
+	if (a==b){
+		console.log (a,b);
+	}
+	/** end**/
+/**	switch case
+var num=9;
+switch(num);
+{
+	case 0;
+	console.log("zero");
+	break;
+	case 1;
+	console.log("one");
+	break;
+	case 9;
+	console.log("nine");
+	break;
+}
+**/
+/**while loop
+let a=0;
+while (a<10){
+	console.log(a);
+	a++;
+}
+**/
+/** for loop 
+for(var i=0;i<10;i++){
+	console.log(i)
+}
+**/
+/**function**/
 
 
 
