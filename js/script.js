cb6cf2c0659184d0f7df874cb08edf6e0826aa0b
mugeshkumar <kmugesh11199@gmail.com>
2022-08-25 11:21:59 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -55,6 +55,63 @@ for(var i=0;i<10;i++){
 }
 **/
 /**function**/
+/*array//
+var mark 1=50;
+var mark 2=30;
+var mark 3=40;
+var mark 4=80;
+var mark 5=90
 
+var marks=(mark1+mark2+mark3+mark4+mark5)/5*/
+/* array push//
+var marks=[];
+marks.push(40);
+marks.push(50);
+marks.push(70);
+marks.push(90);
+marks.push(85);
+console.log(marks)*/
 
+/*array indexof//
 
+var marks=[];
+marks.indexOf(40);
+marks.indexOf(50);
+marks.indexOf(70);
+marks.indexOf(90);
+marks.indexOf(85);
+console.log(marks)*/
+
+/*array last index of//
+
+var marks=[];
+marks.lastIndexOf(40);
+marks.lastIndexOf(50);
+marks.lastIndexOf(70);
+marks.lastIndexOf(90);
+marks.lastIndexOf(85);
+console.log(marks);*/
+
+/* array length//
+var marks=[];
+marks.pop(40);
+marks.pop(50);
+marks.pop(70);
+marks.pop(90);
+marks.pop(85);
+console.log(marks);*/
+
+/*loop array//
+var marks=[12,12,13,14,15,16,17,18];
+var total=0;
+var avg;
+for (let i=0; i<marks.length; i++){
+	total=total+marks[i];
+}
+avg=total/marks.length;
+console.log(avg);*/
+
+var user=["one","two","three","four",]
+for (let i=0; i<user.length; i++){
+	console.log(i);
+}
